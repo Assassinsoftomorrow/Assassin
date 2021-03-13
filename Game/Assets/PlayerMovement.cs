d08ffdb8d0cb8e92c0dadcfe6f170a905987dd9b
Kushagra <kushagra502@gmail.com>
2021-03-14 00:47:45 +0530

    Fixed player movement code

---
+++ Game/Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movespeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    // Used for input
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    // Used for movement
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+    }
+}

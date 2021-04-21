fa303a97154f0e9c546d4ca9585b29fd8e7f67c5
Aint Mina <henry.pekkermann@hotmail.com>
2021-04-21 07:45:57 +0300

    Copied the tilemap to game scene

--- Game/Assets/Code/Shooting.cs
+++ Game/Assets/Code/Shooting.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
 
-
     public Transform firePoint;
-    public Transform firePointLeft;
     public GameObject bulletPrefab;
 
     public float bulletForce = 7f;
@@ -23,6 +21,7 @@ public class Shooting : MonoBehaviour
     }
 
     void Shoot() {
+
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         
